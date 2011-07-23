5d15837d1ef9ce175ac1af2e85f34e8403558b90
SHIOYA, Hiromu <kwappa.856@gmail.com>
2011-07-23 16:53:36 +0900

    add install script
     * add script to create symlink to ~/
     * replace .gitconfig and .gitignore for global setting

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+files=(.zshrc .screenrc .gitconfig .gitignore)
+num=${#files[@]}
+i=0
+while (($i<$num))
+do
+  echo ${files[$i]}
+  ln -s `pwd`/${files[$i]} ~/${files[$i]}
+  i=$i+1
+done
