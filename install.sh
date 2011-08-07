7e2fe62ca22778077996cd5181ba930ed138248a
SHIOYA, Hiromu <kwappa.856@gmail.com>
2011-08-07 12:33:35 +0900

    add .autotest
     * add .autotest
     * add loop for get growl notify image
      * http://cocooooooon.com/kinotan/

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files=(.zshrc .screenrc .gitconfig .gitignore .rspec)
+files=(.zshrc .screenrc .gitconfig .gitignore .rspec .autotest)
 num=${#files[@]}
 i=0
 while (($i<$num))
@@ -8,3 +8,17 @@ do
   ln -s `pwd`/${files[$i]} ~/${files[$i]}
   i=$i+1
 done
+
+if [[ ! -d $HOME/\.autotest\.d ]] ; then
+  mkdir $HOME/.autotest.d
+  autotest_imgs=(rails_fail.png rails_ok.png rails_pending.png)
+  num=${#autotest_imgs[@]}
+  i=0
+  echo $num
+  while (($i<$num))
+  do
+      echo ${autotest_imgs[$i]}
+      wget http://cocooooooon.com/kinotan/${autotest_imgs[$i]} -O $HOME/.autotest.d/${autotest_imgs[$i]}
+      i=$i+1
+  done
+fi
