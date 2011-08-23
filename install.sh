4ec9119dfd6c579b341abdfa3b7f8e8a366e94fa
SHIOYA, Hiromu <kwappa.856@gmail.com>
2011-08-23 20:03:01 +0900

    add .tmux.conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files=(.zshrc .screenrc .gitconfig .gitignore .rspec .autotest .vimrc)
+files=(.zshrc .screenrc .gitconfig .gitignore .rspec .autotest .vimrc .tmux.conf)
 num=${#files[@]}
 i=0
 while (($i<$num))
