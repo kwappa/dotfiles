dbeee4d44fc52b4e616465c114a270cc8e679faa
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-03-05 11:10:15 +0900

    add .pryrc
     * add alias for pry-debugger

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files=(.zshrc .screenrc .gitconfig .gitignore .rspec .autotest .vimrc .tmux.conf .inputrc .ackrc)
+files=(.zshrc .screenrc .gitconfig .gitignore .rspec .autotest .vimrc .tmux.conf .inputrc .ackrc .pryrc)
 num=${#files[@]}
 i=0
 while (($i<$num))
