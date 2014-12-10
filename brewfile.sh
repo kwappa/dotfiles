7e00840be2cd80399c6ed7f2086783a84daf6d97
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-10 20:23:19 +0900

    add shell script to install tools via homebrew
    * it works instead of Brewfile

---
+++ brewfile.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+brew install autoconf
+brew install automake
+brew install bazaar
+brew install cmake
+brew install emacs --cocoa
+brew linkapps
+brew install fontconfig
+brew install freetype
+brew install gdbm
+brew install gettext
+brew install gibo
+brew install git
+brew install go
+brew install hub
+brew install icu4c
+brew install imagemagick
+brew install jpeg
+brew install libevent
+brew install libiconv
+brew install libpng
+brew install libtiff
+brew install libtool
+brew install libxml2
+brew install markdown
+brew install openssl
+brew install pcre
+brew install pkg-config
+brew install rbenv
+brew install readline
+brew install ruby-build
+brew install the_silver_searcher
+brew install tmux
+brew install wget
+brew install xz
+brew install zsh
