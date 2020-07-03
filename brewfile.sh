6108f1211010ec351e7da3f3fbcf02eafd920156
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-07-03 16:45:46 +0900

    :new: Update for macOS Catalina

--- brewfile.sh
+++ brewfile.sh
@@ -1,43 +1,18 @@
 #!/bin/sh
+
 brew update
-brew install autoconf
-brew install automake
-brew install cmake
-brew cask install emacs
-brew install gdbm
-brew install gettext
-brew install git
-brew install go
-brew install hub
-brew install icu4c
-brew install imagemagick
-brew install jpeg
-brew install libevent
-brew install libiconv
-brew install libpng
-brew install libtiff
-brew install libtool
-brew install libxml2
-brew install markdown
-brew install openssl
-brew install pcre
-brew install pkg-config
-brew install python
-brew install rbenv
-brew install readline
-brew install ruby-build
-brew install the_silver_searcher
-brew install tmux
-brew install reattach-to-user-namespace
-brew install wget
-brew install xz
 brew install zsh
 brew install zsh-completions
-brew install pwgen
-
+brew install tmux
+brew cask install emacs
 brew tap homebrew/cask-fonts
 brew cask install font-ricty-diminished
-
+brew install git
+brew install rbenv
+brew install go
+brew install python
+brew install the_silver_searcher
+brew install wget
 brew install ghq
 brew install peco
 go get -u github.com/nsf/gocode
