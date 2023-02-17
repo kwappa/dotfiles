489e6ed00f2260d704ec6dad4ce5f374a4c9909d
Hiromu Shioya <kwappa.856@gmail.com>
2023-02-18 08:48:23 +0900

    :bug: Fix obsolated command of homebrew
    * Also add VS Code

--- brewfile.sh
+++ brewfile.sh
@@ -4,8 +4,6 @@ brew update
 brew install zsh
 brew install zsh-completions
 brew install tmux
-brew tap homebrew/cask-fonts
-brew cask install font-ricty-diminished
 brew install git
 brew install rbenv
 brew install go
@@ -14,6 +12,10 @@ brew install wget
 brew install ghq
 brew install peco
 go get -u github.com/nsf/gocode
+
+brew tap homebrew/cask-fonts
+brew install --cask font-ricty-diminished
+brew install --cask visual-studio-code
 
 brew upgrade
 brew cleanup
